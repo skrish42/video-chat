0cafea57155ab58d34d30e312d1ea97ab4f80a8b
Krishna Kumar S <100086768+skrish42@users.noreply.github.com>
2022-08-25 16:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const app = require("express")();
+const server = require("http").createServer(app);
+import cors from "cors";
 
-import App from './App';
-import { ContextProvider } from './Context';
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
 
-import './styles.css';
+app.use(cors());
 
-ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-  document.getElementById('root'),
-);
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Server is Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
